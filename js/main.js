994110f535a335461960e1ea287c4f2eb695d66f
Liam Ellis <liamhellis@gmail.com>
2016-05-15 00:38:21 -0700

    first full mock-up, not fully responsive

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// $(window).scroll(function(){
+//   var scroll = $(window).scrollTop();
+//   var scrollEffect = $('#scroll-effect');
+//   var nonScrollEffect = $('#non-scroll');
+  
+//   if (scroll >= 30) {
+    
+//     $(nonScrollEffect).slideUp();
+//     $(scrollEffect).slideDown(1000);
+
+//   }
+  
+
+//   // if(scroll >= 20) stickyLogo.addClass('scroll-effect');
+// });
+
